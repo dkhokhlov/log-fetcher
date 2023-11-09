9fa2fb73895276f29cb5514cff6cb0fe55da48fe
Dmitri Khokhlov <dkhokhlov@gmail.com>
2023-11-09 14:57:38 -0800

    fix: handler

--- logs_handler.js
+++ logs_handler.js
@@ -24,47 +24,43 @@ const logger = root_logger.child({id: 'logs_handler'});
  * @returns {Promise<void>}
  */
 async function logs_handler(file_path, file_encoding, chunk_size, num_lines, keyword, async_output) {
-
     const file_stat = await fs.promises.stat(file_path);
-    assert(file_stat.isFile(), file_path, 'The path must be a file');
+    assert(file_stat.isFile(), `The path must be a file: ${file_path}`);
 
     const fd = await fs.promises.open(file_path, 'r');
-    let position = (await fd.stat()).size;
+    let position = file_stat.size;
     let buffer = Buffer.alloc(0);
     let line_count = 0;
     const NEWLINE = 10; // ASCII code for '\n'
 
     try {
         while (position > 0 && (num_lines === undefined || line_count < num_lines)) {
-            const new_chunk_size = Math.min(chunk_size, position);
-            const chunk_buffer = Buffer.alloc(new_chunk_size);
-            position -= new_chunk_size;
-            // read chunk as bytes
-            await fd.read(chunk_buffer, 0, new_chunk_size, position);
-            buffer = Buffer.concat([chunk_buffer, buffer]);
-            let last_line_end = 0;
-            for (let i = 0; i < buffer.length; i++) {
-                // check for newline byte
-                if (buffer[i] === NEWLINE) {
-                    let line = buffer.slice(last_line_end, i);
-                    last_line_end = i + 1; // update the start of the next line after the newline byte
-                    if (keyword)
-                    {
-                        let line_str = line.toString(file_encoding);
-                        if (!line_str.includes(keyword)) continue;
-                    }
+            const read_chunk_size = Math.min(chunk_size, position);
+            const chunk_buffer = Buffer.alloc(read_chunk_size);
+            position -= read_chunk_size;
+            // Use fs.promises.read with the FileHandle object
+            const {bytesRead} = await fd.read(chunk_buffer, 0, read_chunk_size, position);
+            buffer = Buffer.concat([chunk_buffer.slice(0, bytesRead), buffer]);
+            // search for line ends
+            let new_line_index = buffer.indexOf(NEWLINE); // Search for newline
+            while (new_line_index !== -1) {
+                const line = buffer.slice(0, new_line_index);
+                buffer = buffer.slice(new_line_index + 1); // +1 to remove the newline character
+                if (!keyword || line.toString(file_encoding).includes(keyword)) {
                     await async_output(line); // output the line as Buffer
                     line_count++;
                     if (num_lines !== undefined && line_count >= num_lines) {
                         break;
                     }
                 }
+                new_line_index = buffer.indexOf(NEWLINE); // Search for the next newline
             }
-            // keep only the incomplete line at the end for the next iteration
-            buffer = buffer.slice(last_line_end);
         }
+    } catch (error) {
+        logger.error(`An error occurred while reading the file: ${error.message}`);
+        throw error;
     } finally {
-        if (fd) await fd.close();
+        await fd.close();
     }
 }
 
